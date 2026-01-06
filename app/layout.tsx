24f4ed026f2afed900a0e48048ba3badbfc279a3
phil009 <philipogboh60@gmail.com>
2026-01-06 16:32:47 +0100

    removed ai-widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,73 +27,77 @@ import AIChatWidget from "@/components/ai-chatbot";
 // });
 
 export const metadata: Metadata = {
-  title: "Oblique Path | AI Automation for Growing Businesses",
-  description:
-    "Obliquepath helps small to mid-sized businesses automate workflows with custom AI-powered tools—from chatbots to smart inventory systems—to boost efficiency and cut costs.",
-  keywords: [
-    "AI automation",
-    "business automation",
-    "custom AI solutions",
-    "workflow automation",
-    "chatbots for business",
-    "AI tools for SMBs",
-  ],
-  authors: [{ name: "ObliquePath", url: "https://obliquepath.dev" }],
-  creator: "ObliquePath",
-  robots: {
-    index: true,
-    follow: true,
-    nocache: false,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: false,
+    title: "Oblique Path | AI Automation for Growing Businesses",
+    description:
+        "Obliquepath helps small to mid-sized businesses automate workflows with custom AI-powered tools—from chatbots to smart inventory systems—to boost efficiency and cut costs.",
+    keywords: [
+        "AI automation",
+        "business automation",
+        "custom AI solutions",
+        "workflow automation",
+        "chatbots for business",
+        "AI tools for SMBs",
+    ],
+    authors: [{ name: "ObliquePath", url: "https://obliquepath.dev" }],
+    creator: "ObliquePath",
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+        googleBot: {
+            index: true,
+            follow: true,
+            noimageindex: false,
+        },
     },
-  },
-  icons: {
-    icon: "/icon.png",
-  },
-  openGraph: {
-    title: "Oblique Path | AI Automation for Growing Businesses",
-    description:
-      "Unlock your business potential with AI-driven automation—from chatbots to workflow tools. Trusted by SMBs across industries.",
-    url: "https://obliquepath.dev",
-    siteName: "ObliquePath",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Oblique Path | AI Automation for Growing Businesses",
-    description:
-      "Custom AI tools and chatbots to automate business workflows. Boost productivity, cut costs, and scale faster.",
-    // creator: "@yourTwitterHandle", // Optional
-  },
-  metadataBase: new URL("https://obliquepath.dev"),
-  alternates: {
-    canonical: "/",
-  },
+    icons: {
+        icon: "/icon.png",
+    },
+    openGraph: {
+        title: "Oblique Path | AI Automation for Growing Businesses",
+        description:
+            "Unlock your business potential with AI-driven automation—from chatbots to workflow tools. Trusted by SMBs across industries.",
+        url: "https://obliquepath.dev",
+        siteName: "ObliquePath",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Oblique Path | AI Automation for Growing Businesses",
+        description:
+            "Custom AI tools and chatbots to automate business workflows. Boost productivity, cut costs, and scale faster.",
+        // creator: "@yourTwitterHandle", // Optional
+    },
+    metadataBase: new URL("https://obliquepath.dev"),
+    alternates: {
+        canonical: "/",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className="overflow-x-hidden"
-        // className={`${inter.variable} ${poppins.variable} ${firaCode.variable} font-sans`}
-      >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Navbar />
-          {children}
-          <Footer />
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body
+                className="overflow-x-hidden"
+                // className={`${inter.variable} ${poppins.variable} ${firaCode.variable} font-sans`}
+            >
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                >
+                    <Navbar />
+                    {children}
+                    <Footer />
 
-          <Toaster />
-        </ThemeProvider>
-        <AIChatWidget />
-      </body>
-    </html>
-  );
+                    <Toaster />
+                </ThemeProvider>
+                {/* <AIChatWidget /> */}
+            </body>
+        </html>
+    );
 }
